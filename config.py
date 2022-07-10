18795835a871c134cf722ae3fd487bc8d4cf3a32
boomb0om <igorekandco1@gmail.com>
2022-07-10 13:37:06 +0300

    Add some comments

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_key = ""
-test_api_key = ""
+api_key = "your_api_key_here"
+test_api_key = "your_api_key_for_other_bot_instance_here"
 
-ADMIN_IDS = []
+ADMIN_IDS = [] # your telegram id here
 
 network_error_timeout = 10 # timeout in seconds for safe senders if network error occured
